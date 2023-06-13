7ceec9b0e60815ca8fa78c0fd7ffe63e87b72dd9
vadwwxz <vadwwxz@gmail.com>
2023-06-13 17:59:35 +0300

    Prepared application for first release

--- app/src/main/java/com/main/notelink/features/notes/presentation/ui/NotesFragment.kt
+++ app/src/main/java/com/main/notelink/features/notes/presentation/ui/NotesFragment.kt
@@ -1,6 +1,8 @@
 package com.main.notelink.features.notes.presentation.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
@@ -22,6 +24,11 @@ class NotesFragment : BaseFragment<NotesViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+//        Log.d("MyLog", "NotesFragment: send intent success")
+//
+//        val intent = Intent("com.main.notelink.DATA_UPDATED")
+//        requireActivity().sendBroadcast(intent)
 
         binding.rvNotes.layoutManager = StaggeredGridLayoutManager(2, 1)
         binding.rvNotes.adapter = adapter
--- app/src/main/java/com/main/notelink/features/notes/presentation/widget/MainNoteWidget.kt
+++ app/src/main/java/com/main/notelink/features/notes/presentation/widget/MainNoteWidget.kt
@@ -1,57 +1,72 @@
 package com.main.notelink.features.notes.presentation.widget
 
-import android.annotation.SuppressLint
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import android.widget.RemoteViews
-import android.widget.RemoteViewsService
 import androidx.room.Room
 import com.main.notelink.R
 import com.main.notelink.main.data.cache.NoteLinkDatabase
-import com.main.notelink.main.data.cache.dao.NotesDao
-
-class Something : RemoteViewsService() {
-    override fun onGetViewFactory(intent: Intent?): RemoteViewsFactory {
-        TODO("Not yet implemented")
-    }
-}
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainNoteWidget : AppWidgetProvider() {
-    private lateinit var database: NoteLinkDatabase
-    private lateinit var notesDao: NotesDao
-
+    // todo add widget
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(notesDao, context, appWidgetManager, appWidgetId)
+            updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
-    override fun onEnabled(context: Context) {
-        database = Room.databaseBuilder(
-            context,
-            NoteLinkDatabase::class.java,
-            "note_link_database"
-        ).fallbackToDestructiveMigration().build()
-        notesDao = database.notesDao()
+    override fun onReceive(context: Context?, intent: Intent?) {
+        super.onReceive(context, intent)
+        if (intent?.action == "com.main.notelink.DATA_UPDATED") {
+            context?.let { mainContext ->
+                Log.d("MyLog", "MainNoteWidget: received intent")
+                val appWidgetManager = AppWidgetManager.getInstance(mainContext)
+                val appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    ComponentName(mainContext, MainNoteWidget::class.java)
+                )
+                onUpdate(mainContext, appWidgetManager, appWidgetIds)
+            }
+        }
     }
 }
 
-@SuppressLint("RemoteViewLayout")
+@OptIn(DelicateCoroutinesApi::class)
 internal fun updateAppWidget(
-    notesDao: NotesDao,
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    Log.d("MyLog", "smt")
-    Log.d("MyLog", "AddNoteWidget: ${notesDao.getAllNotes().joinToString()}")
-    val views = RemoteViews(context.packageName, R.layout.add_note_widget)
-    appWidgetManager.updateAppWidget(appWidgetId, views)
+    val database = Room.databaseBuilder(
+        context,
+        NoteLinkDatabase::class.java,
+        "note_link_database"
+    ).fallbackToDestructiveMigration().build()
+    val notesDao = database.notesDao()
+
+    GlobalScope.launch(Dispatchers.IO) {
+        val notes = notesDao.getAllNotes().reversed()
+        val views = RemoteViews(context.packageName, R.layout.add_note_widget).also {
+            it.setTextViewText(R.id.firstTitle, notes[0].title)
+            it.setTextViewText(R.id.secondTitle, notes[1].title)
+            it.setTextViewText(R.id.thirdTitle, notes[2].title)
+            it.setTextViewText(R.id.fourthTitle, notes[3].title)
+            it.setTextViewText(R.id.firstContent, notes[0].content)
+            it.setTextViewText(R.id.secondContent, notes[1].content)
+            it.setTextViewText(R.id.thirdContent, notes[2].content)
+            it.setTextViewText(R.id.fourthContent, notes[3].content)
+        }
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
 }
